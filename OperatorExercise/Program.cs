6d23f0daba24c761b73e045c94f0a50cae7090d4
john morgangit config --global user.email jmm915@gmail.com <jmm915@gmail.com>
2021-12-16 13:50:05 -0600

    operators

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+
+            int quotient = a / b;
+            int remainder = a % b;
+
+            Console.WriteLine($"{a}/{b} = {quotient} remainder {remainder}");
+            
+        }
+
+    }
+}
